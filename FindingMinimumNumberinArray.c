85cb9b903d2a6278ffa9b02d47bc8d1be08b0869
TaklaciGuverin <110823265+HTYilmaaz@users.noreply.github.com>
2023-11-23 23:48:31 +0300

    Update FindingMinimumNumberinArray.c
    Find the Minimum Number in an Array | C Programming Example

--- FindingMinimumNumberinArray.c
+++ FindingMinimumNumberinArray.c
@@ -3,13 +3,13 @@
 int main()
 {
     
-int arr[]={5,4,3,2,1,6};
+int arr[]={15,7,4,11,11,2};
 
 int min=arr[0];
 
-for(int i=0; i<6; i++){
+for(int i=1; i<6; i++){
     if(arr[i]<min) min=arr[i];
-    
+    printf("new min is %d\n",min);//to check if its working correctly
 }
     printf("minumum number is %d",min);
     
