7d14ed7baf8d872cadf956351510e7c444638b8f
TaklaciGuverin <110823265+HTYilmaaz@users.noreply.github.com>
2023-11-25 23:05:34 +0300

    Update FindingMinimumNumberinArray.c

--- FindingMinimumNumberinArray.c
+++ FindingMinimumNumberinArray.c
@@ -2,18 +2,20 @@
 
 int findmin(int arr[],int n){
     int min=arr[0];
-    for(int i=1; i<n; i++){
+    for(int i=0; i<n; i++){
         if(arr[i]<min){
             min=arr[i];
         }
     }
+
     return min;
 }
 
 int main(){
-    int arr[]={15,12,9,11,4,5,8};
-    
-    int x=findmin(arr,7);
-    printf("%d",x);
+    int arr[]={7,5,3,5,8,9};
+    int n= sizeof(arr)/sizeof(arr[0]);
+    int k=findmin(arr,n);
+    printf("your min is %d",k);
     return 0;
+    
 }
